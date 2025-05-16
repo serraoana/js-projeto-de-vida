e4aa1b0d329f1960023dff62cf2e6b9fcc55cddf
serraoana <144172636+serraoana@users.noreply.github.com>
2025-05-16 19:34:45 +0000

    remove

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0; i< botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes; j++){
+            botoes[j].classList.remove("ativo");
+        }
+    }
+}
