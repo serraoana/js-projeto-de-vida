a03874ed6d9bba5d3cb618a5669bdd3193860ea7
serraoana <144172636+serraoana@users.noreply.github.com>
2025-05-30 18:21:50 +0000

    cronometro

--- main.js
+++ main.js
@@ -14,8 +14,12 @@ for(let i=0; i< botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00:00");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00:00");
+const tempoObjetivo4 = new Date("2026-01-05T00:00:00:00");
 
-contadores[0].textContent = ;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -29,5 +33,16 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-    return dias +" dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
 }
+
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
