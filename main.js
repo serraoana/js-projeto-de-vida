f367d9ff7c1e5eec7964427fd196a865cf83681b
serraoana <144172636+serraoana@users.noreply.github.com>
2025-06-06 19:20:26 +0000

    final

--- main.js
+++ main.js
@@ -42,7 +42,10 @@ function calculaTempo(tempoObjetivo){
 
 function atualizaCronometro(){
     for (let i = 0; i < contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("dias"+ i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+ i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+ i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+ i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 function comecaCronometro(){
