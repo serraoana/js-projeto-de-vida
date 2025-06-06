0f370008088009a80620ad8bf4f9147c3e55b9bb
serraoana <144172636+serraoana@users.noreply.github.com>
2025-06-06 18:23:04 +0000

    retorno

--- main.js
+++ main.js
@@ -33,7 +33,11 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos +" segundos ";
+    if(tempoFinal > 0){
+    return [dias,horas,minutos,segundos];
+    }else{
+        return[0, 0, 0, 0];
+    }
 }
 
 function atualizaCronometro(){
@@ -45,6 +49,6 @@ function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
-//comecaCronometro();
+comecaCronometro();
 
 
